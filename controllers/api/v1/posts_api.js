2a36880ba5ee8800bbbf3a31ea12f171e5e1431c
Mouktika Namde <mouktikanamde712@gmail.com>
2023-01-26 18:00:16 -0800

    Add authntication and autorization for deleting a post

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -21,16 +21,24 @@ module.exports.index = async function (req, res) {
 module.exports.destroy = async function (req, res) {
   try {
     let post = await Post.findById(req.params.id);
-    post.remove();
 
-    await Comment.deleteMany({ post: req.params.id });
+    if (post.user == req.user.id) {
+      post.remove();
 
-    return res.status(200).json({
-      message: "Posts and assciated comments deleted successfully!",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Internal server error",
+      await Comment.deleteMany({ post: req.params.id });
+
+      return res.json(200, {
+        message: "Post and associated comments deleted successfully!",
+      });
+    } else {
+      return res.json(401, {
+        message: "You cannot delete this post!",
+      });
+    }
+  } catch (err) {
+    console.log("********", err);
+    return res.json(500, {
+      message: "Internal Server Error",
     });
   }
 };
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 const postsApi = require("../../../controllers/api/v1/posts_api");
 // this is /posts part : / --> /posts/
 // url: localhost:8000/api/v1/posts
 
+// to prevent session cookies from being generated we are setiing session: false
 router.get("/", postsApi.index);
-router.delete("/:id", postsApi.destroy);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  postsApi.destroy
+);
 
 module.exports = router;

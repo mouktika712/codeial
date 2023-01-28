1ce3141e008b9b3705dac20af867de4694f48c8d
Mouktika Namde <mouktikanamde712@gmail.com>
2023-01-28 13:21:00 -0800

    Configure Nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "mouktikanamde712@gmail.com",
+    pass: "##2624!Yugandhar",
+  },
+});
+
+let renderTemplate = (data, relativePath) => {
+  let mailHTML;
+
+  ejs.renderFile(
+    path.join(__dirname, "../views/mailer", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("Error in rendering the template");
+        requestAnimationFrame;
+      }
+      mailHTML = template;
+    }
+  );
+
+  return mailHTML;
+};
+
+module.exports = {
+  transporter,
+  renderTemplate,
+};
